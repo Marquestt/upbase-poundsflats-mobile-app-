beb63baf0bcc0d58a1042dca712bb0891d5516bb
Marquestt <thatyanecmarques98@live.com>
2024-11-28 11:33:06 -0300

    atualização do componente From e Input, desenvolvimento em andamento

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,10 +6,11 @@ const Form = () => {
     return (
         <form className={styles.form}>
             <div className={styles.container}>
-                <Input type="text" placeholder="Nome do imóvel"/>
+                <Input type="text" placeholder="Nome do imóvel" required="required"/>
                 <p className={styles.text}>O nome do imóvel será exibido na sua tela inicial e na reserva para o hóspede</p>
             </div>
             <TextArea placeholder="Descrição"/>
+            <Input type="text" placeholder="CEP" maxLength="10" pattern="\d{5}-\d{3}" required="required"/>
         </form>
     )
 }
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,8 +1,8 @@
 import styles from './Input.module.css';
 
-const Input = ({type = "text", placeholder = "", width}) => {
+const Input = ({type = "text", placeholder = "", width, maxLength = "", pattern = "", required}) => {
     return (
-        <input className={styles.input} type={type} placeholder={placeholder} style={{ width }}/>
+        <input className={styles.input} type={type} placeholder={placeholder} style={{ width }} required={required}/>
     )
 }
 
